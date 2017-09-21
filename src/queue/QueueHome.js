a6165c6ea2aa7e94f5ef854b87362fd6efc2fb1b
LongSheLA <475992348@qq.com>
2017-09-21 17:38:45 +0800

    “路由”

--- src/queue/QueueHome.js
+++ src/queue/QueueHome.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './../logo.svg';
 import './../App.css';
 import {Link} from 'react-router-dom'
 export default class QueueHome extends Component {
@@ -7,7 +6,7 @@ export default class QueueHome extends Component {
         return (
             <div className="App">
                 <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
+                    {/*<img src={logo} className="App-logo" alt="logo" />*/}
                     <h2>Welcome to React</h2>
                 </div>
                 <div className="centerInfo">
