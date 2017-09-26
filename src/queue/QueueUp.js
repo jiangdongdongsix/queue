c358ea895aa1a079dc0861793aa525a6152fdd3c
LongSheLA <475992348@qq.com>
2017-09-26 17:41:53 +0800

    “header”

--- src/queue/QueueUp.js
+++ src/queue/QueueUp.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Layout,Input, Icon, Row, Col } from 'antd';
+import MyHeader from './../component/Header/Header';
 
 const { Header, Footer, Sider, Content } = Layout;
 class QueueUp extends React.Component{
@@ -21,8 +22,10 @@ class QueueUp extends React.Component{
         const { userName } = this.state;
         const suffix = userName ? <Icon type="close-circle" onClick={this.emitEmpty} /> : null;
         return (
+
             <div className="my-app">
-                <Layout style={{backgroundColor: 'lightgrey'}}>
+                <Layout>
+                <MyHeader name="排队取号"/>
                 <Content>
                 <Input
                     placeholder="请输入用餐人数"
@@ -55,16 +58,46 @@ class QueueUp extends React.Component{
                         </Col>
                     </Row>
                 </div>
-                    <Input
-                        placeholder="请输入手机号"
-                        prefix={<Icon type="mobile" />}
-                        suffix={<Icon type="right" />}
-                        size="large"
-                        value={userName}
-                        onChange={this.onChangeUserName}
-                        ref={node => this.userNameInput = node}
-                    />
+                    <div>
+                        <Input
+                            placeholder="请输入手机号"
+                            prefix={<Icon type="mobile" />}
+                            suffix={<Icon type="right" />}
+                            size="large"
+                            value={userName}
+                            onChange={this.onChangeUserName}
+                            ref={node => this.userNameInput = node}
+                        />
+                    </div>
+
                 </Content>
+                    <Footer>
+                        <div className="keyborard clearFloat" style={{backgroundColor:'white'}}>
+                            <div className="marginSet">
+                                <div className="keyborardContent clearFloat">
+                                    <div className="keyborardLeft clearFloat" id="keyborardNumber">
+                                        <div><input type="button" value="1"/></div>
+                                        <div><input type="button" value="2"/></div>
+                                        <div><input type="button" value="3"/></div>
+                                        <div><input type="button" value="4"/></div>
+                                        <div><input type="button" value="5"/></div>
+                                        <div><input type="button" value="6"/></div>
+                                        <div><input type="button" value="7"/></div>
+                                        <div><input type="button" value="8"/></div>
+                                        <div><input type="button" value="9"/></div>
+                                        <div><input type="button" value="清空"/></div>
+                                        <div><input type="button" value="0"/></div>
+                                        <div><input type="button" value="X"/></div>
+                                    </div>
+                                    <div className="keyborardRight" >
+                                        <div>
+                                            <input type="button" value="立即取号"/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Footer>
                 </Layout>
             </div>
 
