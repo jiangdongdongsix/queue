e7c8c6256e7d15907f24cb78a93d3a9d46e2751f
lijingyanyan <yanlijing.tp@advantech.com.cn>
2017-10-11 14:45:18 +0800

    revision

--- src/component/Time/Time.js
+++ src/component/Time/Time.js
@@ -1,35 +1,32 @@
-import React,{ Component } from 'react'
+import React,{ Component } from 'react';
 import { Row,Col } from 'antd';
-import './time.css'
 
-export default class Time extends Component{
+export default class Clock extends Component{
     constructor(){
         super();
-        var date = new Date();
         this.state = {
             date:new Date()
         }
     }
-    componentWillMount(){
+
+    componentWillmount(){
         this.timer = setInterval(()=>{
-            this.setState({ date:new Date() })
+            this.setState({ date:new Date })
         },1000)
     }
 
-        componentWillUnMount(){
-            clearInterval(this.timer);
-        }
+    componentWillUnmount () {
+        clearInterval(this.timer)
+    }
     render(){
-        return(
+        return (
             <Row>
                 <Col span={22}></Col>
                 <Col span={2}>
-                    {/*<h2>{this.state.hour}:{this.state.minute}</h2>*/}
-                    {/*<p>{this.state.year}/{this.state.month}/{this.state.day}</p>*/}
                     <h2>{this.state.date.toLocaleTimeString().substring(2)}</h2>
                     <p>{this.state.date.toLocaleDateString()}</p>
                 </Col>
             </Row>
-        );
+        )
     }
 }
