cde1bade35311e9f96dd65d6d9df0b94923efb52
lijingyanyan <yanlijing.tp@advantech.com.cn>
2017-10-16 15:11:29 +0800

    'revision'

--- src/component/Time/Time.js
+++ src/component/Time/Time.js
@@ -1,7 +1,7 @@
 import React,{ Component } from 'react';
 import { Row,Col } from 'antd';
 
-export default class Clock extends Component{
+export default class Time extends Component{
     constructor(){
         super();
         this.state = {
@@ -9,7 +9,7 @@ export default class Clock extends Component{
         }
     }
 
-    componentWillmount(){
+    componentWillMount(){
         this.timer = setInterval(()=>{
             this.setState({ date:new Date })
         },1000)
@@ -21,8 +21,8 @@ export default class Clock extends Component{
     render(){
         return (
             <Row>
-                <Col span={22}></Col>
-                <Col span={2}>
+                <Col span={21}></Col>
+                <Col span={3}>
                     <h2>{this.state.date.toLocaleTimeString().substring(2)}</h2>
                     <p>{this.state.date.toLocaleDateString()}</p>
                 </Col>
--- src/queue/QueueVerify.js
+++ src/queue/QueueVerify.js
@@ -63,85 +63,73 @@ export default class QueueVerify extends Component{
         return (
                 <div className='Verify'>
                 <GridHeader name='入场验证'/>
-                <div>
-                    <Row>
+                    <Row style={{height:'89%'}}>
                         <Col span={8}></Col>
-                        <Col span={8} className='Verify-method'>
-                            <h3>方式一</h3>
-                        </Col>
-                    </Row>
-                    <Row className='Verify-method1'>
-                        <Col span={8}></Col>
-                        <Col span={1}>
-                            <Icon type="mobile" style={{fontSize:18,paddingTop:3}}/>
-                        </Col>
-                        <Col span={2}>
-                            <h3>排号:</h3>
-                        </Col>
-                        <Col span={5}>
-                            <Input placeholder='请输入排号'
-                                   style={{border:0}} onFocus={this.handleNumber.bind(this)}
-                                   value={queueNumber}
-                                   onChange={this.onChangeUserName.bind(this)}/>
-                        </Col>
-                        <Col span={8}></Col>
-                    </Row>
-                </div>
-                <div>
-                    <Row>
-                        <Col span={8}></Col>
-                        <Col span={8} className='Verify-method'>
-                            <h3>方式二</h3>
-                        </Col>
-                    </Row>
-                    <Row className='Verify-method2'>
-                        <Col span={8}></Col>
-                        <Col span={8}><p>请将您收到的二维码，放置感应区进行扫描</p></Col>
-                        <Col span={8}></Col>
-                    </Row>
-                    <Row>
-                        <Col span={8}></Col>
-                        <Col span={8} style={{textAlign:'center'}}>
-                            <img src={verify} alt='' style={{width:'30%',height:'30%'}}/>
-                        </Col>
-                        <Col span={8}></Col>
-                    </Row>
-                </div>
-                <div>
-                    <Row>
-                        <Col span={8}></Col>
-                        <Col span={8} className='Button-panel'>
-                        <div className="clearFloat" style={{marginTop:'10px'}}>
-                        <div className="marginSet">
-                            <div className="keyborardContent clearFloat">
-                                <div className="keyborardLeft clearFloat" id="keyborardNumber" style={{width:'70%',marginLeft:'50px'}}>
-                                    <div><input onClick={this.addNumber.bind(this)} type="button" value="1"/></div>
-                                    <div><input type="button" value="2"/></div>
-                                    <div><input type="button" value="3"/></div>
-                                    <div><input type="button" value="4"/></div>
-                                    <div><input type="button" value="5"/></div>
-                                    <div><input type="button" value="6"/></div>
-                                    <div><input type="button" value="7"/></div>
-                                    <div><input type="button" value="8"/></div>
-                                    <div><input type="button" value="9"/></div>
-                                    <div><input type="button" value="清空"/></div>
-                                    <div><input type="button" value="0"/></div>
-                                    <div><input type="button" value="X"/></div>
-                                </div>
-                                <div className="keyborardRight" style={{width:'10%'}} >
-                                    <div>
-                                        <input type="button" value="验证" onClick={this.verify.bind(this)}/>
+                        <Col span={8} className='Media-width' style={{height:'100%'}}>
+                            <Row>
+                                <Col span={24} className='Verify-method'>
+                                    <h3>方式一</h3>
+                                </Col>
+                            </Row>
+                            <Row className='Verify-method1'>
+                                <Col span={1}>
+                                    <Icon type="mobile" style={{fontSize:18,paddingTop:3}}/>
+                                </Col>
+                                <Col span={5}>
+                                    <h3>排号:</h3>
+                                </Col>
+                                <Col span={18}>
+                                    <Input placeholder='请输入排号'
+                                           style={{border:0}} onFocus={this.handleNumber.bind(this)}
+                                           value={queueNumber}
+                                           onChange={this.onChangeUserName.bind(this)}/>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col span={24} className='Verify-method'>
+                                    <h3>方式二</h3>
+                                </Col>
+                            </Row>
+                            <Row className='Verify-method2'>
+                                <Col span={24}><p>请将您收到的二维码，放置感应区进行扫描</p></Col>
+                            </Row>
+                            <Row>
+                                <Col span={24} style={{textAlign:'center'}}>
+                                    <img src={verify} alt='' className="Verify-img"/>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col span={24} className='Button-panel'>
+                                    <div className="clearFloat" style={{marginTop:'10px'}}>
+                                        <div className="marginSet">
+                                            <div className="keyborardContent clearFloat">
+                                                <div className="keyborardLeft clearFloat" id="keyborardNumber" style={{width:'70%',marginLeft:'50px'}}>
+                                                    <div><input onClick={this.addNumber.bind(this)} type="button" value="1"/></div>
+                                                    <div><input type="button" value="2"/></div>
+                                                    <div><input type="button" value="3"/></div>
+                                                    <div><input type="button" value="4"/></div>
+                                                    <div><input type="button" value="5"/></div>
+                                                    <div><input type="button" value="6"/></div>
+                                                    <div><input type="button" value="7"/></div>
+                                                    <div><input type="button" value="8"/></div>
+                                                    <div><input type="button" value="9"/></div>
+                                                    <div><input type="button" value="清空"/></div>
+                                                    <div><input type="button" value="0"/></div>
+                                                    <div><input type="button" value="X"/></div>
+                                                </div>
+                                                <div className="keyborardRight" style={{width:'10%'}} >
+                                                    <div>
+                                                        <input type="button" value="验证" onClick={this.verify.bind(this)}/>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                                </Col>
+                            </Row>
                         </Col>
-                        <Col span={8}></Col>
                     </Row>
-
                 </div>
-            </div>
         );
     }
 }
